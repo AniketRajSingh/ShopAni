aa13ae3b0bb5b90c9148e63c8ac2f7515a77802d
Aniket Raj Singh <mohitrajingh223@gmail.com>
2022-09-21 20:53:28 +0530

    ShopAni Project Started

---
+++ ShopAni/ShopAniApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShopaniappConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ShopAniApp"
---
+++ ShopAni/ShopAniBlog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShopaniblogConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ShopAniBlog"
